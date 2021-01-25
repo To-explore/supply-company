0ffc4adbd5699736fc66d59aa0bda026e31628bd
Fxhani <66287756+FXhani@users.noreply.github.com>
2021-01-25 16:53:53 +0800

    测试
    测试

--- src/js/index.js
+++ src/js/index.js
@@ -4,12 +4,16 @@
  * @LastEditTime: 2021-01-16 16:20:38
  */
 var sayHello = "欢迎来到中商云产业链供应链管理平台！";
-console.log("sayHello :>> ", sayHello);
-var mySwiper = new Swiper(".swiper-container", {
-    direction: "horizontal",
-    loop: true,
-    // 如果需要分页器
-    pagination: {
-        el: ".swiper-pagination",
-    },
-});
+console.log(sayHello);
+console.log("严肃声明：");
+console.log("本系统已申请软件著作权，受国家版权局知识产权以及国家计算机软件著作权保护！");
+console.log("可正常分享和学习源码，不得用于违法犯罪活动，违者必究！");
+console.log("版权所有，侵权必究！");
+// var mySwiper = new Swiper(".swiper-container", {
+//     direction: "horizontal",
+//     loop: true,
+//     // 如果需要分页器
+//     pagination: {
+//         el: ".swiper-pagination",
+//     },
+// });
